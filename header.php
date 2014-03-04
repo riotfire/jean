ac58451118e1baca45684a095563f0aa4d6b0894
Dustin Hoyman <riotfire@gmail.com>
2014-03-03 18:51:32 -0700

    removed Reverie Icons

--- header.php
+++ header.php
@@ -22,16 +22,18 @@
 	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-retina.png" />
 	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon.png" />
 
-	<!-- Enable Startup Image for iOS Home Screen Web App -->
+	<!-- // Enable Startup Image for iOS Home Screen Web App 
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/mobile-load.png" />
 
-	<!-- Startup Image iPad Landscape (748x1024) -->
+	<?php // Startup Image iPad Landscape (748x1024) ?> 
 	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)" />
-	<!-- Startup Image iPad Portrait (768x1004) -->
+	<?php // Startup Image iPad Portrait (768x1004) ?>
 	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" />
-	<!-- Startup Image iPhone (320x460) -->
+	<?php // Startup Image iPhone (320x460) ?>
 	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load.png" media="screen and (max-device-width: 320px)" />
+
+	-->
 
 	<link href='http://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300,700,300italic,700italic' rel='stylesheet' type='text/css'>
 <?php wp_head(); ?>
